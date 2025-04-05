469ee9afe8827a9b113e943fa0c48610405b17b4
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-04-06 01:49:09 +0530

    src: expand token for 1 posn

--- scanner.go
+++ scanner.go
@@ -30,3 +30,43 @@ func (s *Scanner) ScanTokens() []Token {
 func (s Scanner) IsAtEnd() bool {
 	return s.current >= len(s.source)
 }
+
+//in each turn of the loop, we scan a single token.
+// imagine if every lexeme were only a single character long.
+
+func (s Scanner) ScanToken() {
+	c := s.advance()
+
+	switch c {
+	case '(':
+		s.addToken(LEFT_PAREN, nil)
+	case ')':
+		s.addToken(RIGHT_PAREN, nil)
+	case '{':
+		s.addToken(LEFT_BRACE, nil)
+	case '}':
+		s.addToken(RIGHT_BRACE, nil)
+	case ',':
+		s.addToken(COMMA, nil)
+	case '.':
+		s.addToken(DOT, nil)
+	case '-':
+		s.addToken(MINUS, nil)
+	case '+':
+		s.addToken(PLUS, nil)
+	case ';':
+		s.addToken(PLUS, nil)
+	case '*':
+		s.addToken(STAR, nil)
+	}
+}
+
+func (s Scanner) advance() byte {
+	temp := s.current + 1
+	return s.source[temp]
+}
+
+func (s *Scanner) addToken(tokenType TokenType, literal interface{}) {
+	text := s.source[s.start:s.current]
+	s.tokens = append(s.tokens, NewToken(tokenType, text, literal, s.line))
+}
